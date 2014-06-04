60682279ad7712fd163a8dc99187a7c7ef54ddce
otf <phoenixsingleton@gmail.com>
2014-06-04 20:41:41 +0900

    add lookup test.

---
+++ IrKit.Tests/LookupTest.fs
@@ -0,0 +1,17 @@
+﻿namespace IrKit.Tests
+
+open System
+open NUnit.Framework
+open FsUnit
+open System.Net.Http
+open System.Threading.Tasks
+open IrKit
+
+[<TestFixture>]
+[<Explicit>]
+type LookupTest () =
+  [<TestCase("192.168.1.200")>]
+  member test.``should found the endpoint when lookup-ing`` ip =
+    lookup zeroConfResolver
+    |> Async.RunSynchronously
+    |> should equal [Wifi ip]
