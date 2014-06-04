6d666ac4da08219f3f91301cbc53dbd91a14fcf7
otf <phoenixsingleton@gmail.com>
2014-06-04 13:41:34 +0900

    add test when sending.

---
+++ IrKit.Tests/IrKitTest.fs
@@ -0,0 +1,24 @@
+﻿namespace IrKit.Tests
+
+open NUnit.Framework
+open FsUnit
+open Foq
+open System.Net.Http
+open IrKit
+
+[<TestFixture>]
+type IrKitTest () =
+
+  [<Test>]
+  member test.``should request a msg when sending the msg.`` () =
+    let http : HttpMessageInvoker = Mock.With(fun h -> 
+      <@
+        h.SendAsync(is(fun httpMsg -> httpMsg.Method = HttpMethod.Post), any()) --> null
+      @>)
+
+    { Frequency = 40; Data = [] }
+    |> send http Lookup 
+    |> Async.RunSynchronously
+
+    expect <@ http.SendAsync(any(), any()) @> once
+    verifyAll http
---
+++ IrKit/IrKit.fs
@@ -0,0 +1,16 @@
+﻿namespace IrKit
+
+open System.Net.Http
+
+type DeviceEndPoint = Lookup
+
+type Message = {
+  Frequency : int
+  Data : int list
+}
+
+[<AutoOpen>]
+module IrKitFuncs =
+  let send msg endPoint (http:#HttpMessageInvoker) = async {
+    return ()
+  }
