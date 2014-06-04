8e1b68b11c6286d2d8c41c104e9b5f44f11288ad
otf <phoenixsingleton@gmail.com>
2014-06-04 12:14:15 +0900

    add assemblyinfo.

---
+++ IrKit/AssemblyInfo.fs
@@ -0,0 +1,21 @@
+﻿module AssemblyInfo
+
+#light
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitle("IrKit")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("IrKit")>]
+[<assembly: AssemblyCopyright("Copyright © otf")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+[<assembly: ComVisible(false)>]
+[<assembly: Guid("b71e5824-555d-4f05-83fe-bc79098e7b8d")>]
+[<assembly: AssemblyVersion("0.1.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do()
