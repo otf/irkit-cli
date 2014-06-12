f8cdade7558b42eefc65a6d9943be1fc651e4643
otf <phoenixsingleton@gmail.com>
2014-06-12 21:30:23 +0900

    Revert "remove lookup function."
    This reverts commit 2e01b1e2b601eb62b14d89976e6f3261d5788545.

---
+++ IrKit.Tests/LookupTest.fs
@@ -0,0 +1,17 @@
+﻿namespace IrKit.Tests
+
+open System
+open NUnit.Framework
+open FsUnit
+open System.Net.Http
+open System.Threading.Tasks
+open IrKit
+
+[<TestFixture>]
+[<Explicit>]
+type LookupTest () =
+  [<TestCase("192.168.11.28")>]
+  member test.``should found the endpoint when lookup-ing`` ip =
+    lookup zeroconfResolver
+    |> Async.RunSynchronously
+    |> should contain (Wifi ip)
--- IrKit.Tests/ReceivingTest.fs
+++ IrKit.Tests/ReceivingTest.fs
@@ -35,10 +35,17 @@ type ReceivingTest () =
   member test.``should request getting msg when sending request to the device by looked.`` ip freq data =
     let content =  sprintf @"{""format"":""raw"",""freq"":%d,""data"":%s}" freq data
     let httpMock = createHttpMock ip content
+    let resolve = fun r -> <@ (r:IDeviceEndPointResolver).ResolveAsync() @>
 
+    let resolver = Mock.With(fun r ->
+      <@
+        %(resolve r) --> async.Return [Wifi ip]
+      @>)
+  
     let data = List.ofSeq ((JsonValue.Parse data :?> JsonArray) |> map (fun v -> v.Value.ReadAs<int>()) )
     monad {
-      return! receive httpMock (Wifi ip)
+      let! dev = List.head <!> lookup resolver
+      return! receive httpMock dev
     }
     |> Async.RunSynchronously
     |> should equal ({ Frequency = freq; Data = data })
